e4f994da4c9ae046e3210edb4c26dd1f9e18070f
Rastelli1 <rastelliruth@gmail.com>
2023-05-11 16:37:16 -0300

    Se conecto la escena “Menu” con las instancias de “RedGenerator” y “VioletGenerator”

--- ejercicio_senales/scenes/Menu/Menu.gd
+++ ejercicio_senales/scenes/Menu/Menu.gd
@@ -8,6 +8,8 @@ func _ready():
 	# Inicializa el texto de "CountLabel" usando el texto
 	# Constante "INSTANCES_TEXT" más el valor de "instancesCount" como Texto
 	$CountLabel.text = INSTANCES_TEXT + str(instancesCount)
+	$RedGenerator.connect("button_down",count_new_instance)
+	$VioletGenerator.connect("button_down", count_new_instance)
 	pass
 	
 func count_new_instance():
